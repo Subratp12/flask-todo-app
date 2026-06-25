80257ad282b351126a9ba7862f898043dba27edf
Subratp12 <subrat.padhi1204@gmail.com>
2026-06-25 14:33:34 +0000

    fix: rename resources.tf to provider.tf, fix formatting, add dockerignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "var.repo_name"
+resource "aws_ecr_repository" "to_do" {
+  name                 = var.repo_name
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "ecr_repository_name" {
   description = "The name of the ECR repository"
-  value       = aws_ecr_repository.foo.name
+  value       = aws_ecr_repository.to_do.name
 }
 
 output "ecr_repository_url" {
   description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.foo.repository_url
+  value       = aws_ecr_repository.to_do.repository_url
 }
