789c74fa62a0ae701037605e4a064e62c325b3d1
Subratp12 <subrat.padhi1204@outlook.com>
2026-06-25 16:22:51 +0530

    Add variables for ECR repository and region

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "repo_name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "to-do-image"
+}
+variable "region" {
+description = "Name of the region"
+type = string
+default = "ap-south-1"
+}
